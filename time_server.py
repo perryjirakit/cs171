30cae1f34ef29092db3b5eda33a1ab2faef88053
Perry Thampiratwong <jthampiratwong@ucsb.edu>
2025-10-18 02:13:59 -0700

    trying

--- time_server.py
+++ time_server.py
@@ -16,6 +16,7 @@ def handle_client(conn, addr):
     conn.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
     print("Server is listening...")
